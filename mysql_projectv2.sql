97e9cef0f950a29b9ffc3182781f70eb9289a0b4
Bryan Musungu <bryanmusungu@Bryans-MacBook-Pro.local>
2018-12-02 13:21:43 -0600

    fixed the mysql upload

--- mysql_projectv2.sql
+++ mysql_projectv2.sql
@@ -9,9 +9,9 @@ DROP table if EXISTS `Organism`;
 DROP table if EXISTS `Experimental_validation`;
 
 
-/*Create the ortholog table*/
+/*Create the Ortholog table*/
 
-use data2;
+
 CREATE TABLE `GeneID` (
 	`id` int auto_increment KEY NOT NULL,
 	`NCBI_ProteinID` varchar(255) NOT NULL,
@@ -45,28 +45,57 @@ INSERT into `Organism` (Organism_Type) VALUES (' Mouse'),( 'Chicken'),('Bakers Y
 ('Catfish'),('Zebrafish');
 
 
-select Organism_id from Organism where Organism_Type = "Zebrafish" and Organism_Type is not null;
+/*Modification removed the many to 1 interaction for the pubmed IDS
+The table will only have a unique id for the experiment as well as the Type of the experiment
+NOTE: Remove pubmed from the table statement.alter*/
+CREATE TABLE `Experimental_validation` (
+	`id` int  auto_increment NOT NULL PRIMARY KEY,
+    `Experimental_Type` varchar(255) DEFAULT NULL
+
+)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+
+INSERT into `Experimental_validation` (Experimental_Type) VALUES 
+('Mass Spec'),('Yeast-2-Hybrid'),
+('Pull-down');
 
 
-insert into ortholog (ProteinIDA, Organism, ProteinIDB, Experimental_condition) values 
+
+insert into Ortholog (ProteinIDA, Organism, ProteinIDB, Experimental_condition) values 
 (
-(select id from GeneID where NCBI_ProteinID = "ProteinA11"),
+(select id from GeneID where NCBI_ProteinID = "ProteinA11" and NCBI_ProteinID is not null),
 (select Organism_id from Organism where Organism_Type = "Zebrafish" and Organism_Type is not null),
-(select id from GeneID where NCBI_ProteinID = "ProteinA3"),
-'Yeast-2-hybrid'
+(select id from GeneID where NCBI_ProteinID = "ProteinA3" and NCBI_ProteinID is not null),
+(select id from Experimental_validation where Experimental_Type = "Yeast-2-hybrid" and Experimental_Type is not null)
+);
+
+insert into Ortholog (ProteinIDA, Organism, ProteinIDB, Experimental_condition) values 
+(
+(select id from GeneID where NCBI_ProteinID = "ProteinA11" and NCBI_ProteinID is not null),
+(select Organism_id from Organism where Organism_Type = "Zebrafish" and Organism_Type is not null),
+(select id from GeneID where NCBI_ProteinID = "ProteinA2" and NCBI_ProteinID is not null),
+(select id from Experimental_validation where Experimental_Type = "Yeast-2-hybrid" and Experimental_Type is not null)
+);
+
+insert into Ortholog (ProteinIDA, Organism, ProteinIDB, Experimental_condition) values 
+(
+(select id from GeneID where NCBI_ProteinID = "ProteinA1" and NCBI_ProteinID is not null),
+(select Organism_id from Organism where Organism_Type = "Zebrafish" and Organism_Type is not null),
+(select id from GeneID where NCBI_ProteinID = "ProteinA11" and NCBI_ProteinID is not null),
+(select id from Experimental_validation where Experimental_Type = "Yeast-2-hybrid" and Experimental_Type is not null)
 );
 
 /*Query is testing with selecting the value before inserting*/
 set @ValOne = (select id from GeneID where NCBI_ProteinID = "ProteinA1" and NCBI_ProteinID is not null);
 set @ValTwo = (select id from GeneID where NCBI_ProteinID = "ProteinA4"  and NCBI_ProteinID is not null);
 set @Org = (select Organism_id from Organism where Organism_Type = "Zebrafish" and Organism_Type is not null);
+set @Exp = (select id from Experimental_validation where Experimental_Type = "Yeast-2-hybrid" and Experimental_Type is not null);
 
-insert into ortholog (ProteinIDA, Organism, ProteinIDB, Experimental_condition) values 
+insert into Ortholog (ProteinIDA, Organism, ProteinIDB, Experimental_condition) values 
 (
 (select @ValOne),
 (select @Org),
 (select @ValTwo),
-'Yeast-2-hybrid' /*make into a id later*/
+(select @Exp) /*make into a id later*/
 );
 
 /*The RNA-seq information table*/
@@ -77,23 +106,14 @@ CREATE TABLE `RNA_seq_Sample_info` (
     `Expression` float DEFAULT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-set @Val3 = (select id from geneid where NCBI_ProteinID ='ProteinA1');
-set @Val4 = (select id from geneid where NCBI_ProteinID ='ProteinA4');
-set @Val5 = (select id from geneid where NCBI_ProteinID ='ProteinA2');
+set @Val3 = (select id from GeneID where NCBI_ProteinID ='ProteinA1');
+set @Val4 = (select id from GeneID where NCBI_ProteinID ='ProteinA4');
+set @Val5 = (select id from GeneID where NCBI_ProteinID ='ProteinA2');
 
 INSERT into `RNA_seq_Sample_info`  (ProteinNcbiID, Sample_info, Expression) VALUES ((select  @Val3),'GSE13455',67.1),
 ((select  @Val3),'GSE134545',23.5),((select  @Val3),'GSE13456',89.1);
 
-CREATE TABLE `Experimental_validation` (
-	`id` int  auto_increment NOT NULL PRIMARY KEY,
-    `Experimental_Type` varchar(255) DEFAULT NULL,
-    `PubmedID` int DEFAULT NULL
 
-)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-
-INSERT into `Experimental_validation` (Experimental_Type, PubmedID) VALUES ('Yeast-2-Hybrid',123458),
-('Mass Spec',234566),('Yeast-2-Hybrid',129586),
-('Pull-down',123456);
 
 
 
